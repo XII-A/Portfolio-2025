39325c3fbbe8266c865031660e168df5c8842f82
Abdulrazak Armanazi <abdulrazakarmnazi@gmail.com>
2025-07-06 19:57:02 +0300

    update: add right icon

--- portfolio/src/routes/components/ViewMore.tsx
+++ portfolio/src/routes/components/ViewMore.tsx
@@ -5,7 +5,16 @@ const ViewMore = () => {
         <motion.div className="flex cursor-pointer flex-col gap-1.5" initial="rest" whileHover="hover">
             <div className="flex w-full items-center justify-between text-white">
                 <div>View More</div>
-                <div>{`->`}</div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="1"
+                        d="m18 8l4 4m0 0l-4 4m4-4H2"
+                    />
+                </svg>
             </div>
             <div className="relative">
                 <div className="h-px w-full bg-neutral-600" />
