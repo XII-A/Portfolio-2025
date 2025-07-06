6e5a17d145135188a6d13feedfe7ad6fcef6c16e
Abdulrazak Armanazi <abdulrazakarmnazi@gmail.com>
2025-07-06 17:27:32 +0300

    feat: smooth scroll

--- portfolio/src/routes/__root.tsx
+++ portfolio/src/routes/__root.tsx
@@ -1,10 +1,33 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import ReactLenis, { type LenisRef } from 'lenis/react'
+import { cancelFrame, frame } from 'motion'
+import { useEffect, useRef } from 'react'
 import { SelectedProjectProvider } from '../Providers/SelectedProject'
 
 export const Route = createRootRoute({
-    component: () => (
-        <SelectedProjectProvider>
-            <Outlet />
-        </SelectedProjectProvider>
-    )
+    component: RootLayout
 })
+
+function RootLayout() {
+    const lenisRef = useRef<LenisRef>(null)
+
+    useEffect(() => {
+        function update(data: { timestamp: number }) {
+            const time = data.timestamp
+            lenisRef.current?.lenis?.raf(time)
+        }
+
+        frame.update(update, true)
+
+        return () => cancelFrame(update)
+    }, [])
+
+    return (
+        <>
+            <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+            <SelectedProjectProvider>
+                <Outlet />
+            </SelectedProjectProvider>
+        </>
+    )
+}
